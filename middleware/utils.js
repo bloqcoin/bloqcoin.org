a77be2ef58119e81761390f528574ecb36935389
Kris Henriksen <khe@bloqifi.com>
2022-11-14 20:07:08 +0700

    Added mining pool

--- middleware/utils.js
+++ middleware/utils.js
@@ -6,14 +6,11 @@
  */
 function getDaemon() {
 
-	const parseDbUrl = require('parse-database-url');
-	const nodeConfig = parseDbUrl(process.env.BLOQCOIN_URI);
-
 	return require('../connections/socket.js')()
-	.set('host', nodeConfig.host)
-	.set('port', nodeConfig.port)
-	.set('user', nodeConfig.user)
-	.set('pass', nodeConfig.password);
+	.set('host', process.env.BLOQCOIN_HOST)
+	.set('port', process.env.BLOQCOIN_PORT)
+	.set('user', process.env.BLOQCOIN_USER)
+	.set('pass', process.env.BLOQCOIN_PASS);
 }
 
 module.exports = {
