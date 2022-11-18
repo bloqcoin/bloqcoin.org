09b2d4f931016b89a205a6b386d4c7e6174852f4
Kris Henriksen <khe@bloqifi.com>
2022-11-18 08:32:35 +0700

    clean up, add backup node

--- pool.js
+++ pool.js
@@ -7,152 +7,161 @@ const WebSocket = require('ws');
 
 const Stratum = require('./middleware/stratum-pool');
 const pool = Stratum.createPool({
-
-    "coin": {
+	"coin": {
 		"name": "Bloqcoin",
 		"symbol": "BLOQ",
 		"algorithm": "sha256",
 		"nValue": 1024, //optional - defaults to 1024
 		"rValue": 1, //optional - defaults to 1
 		"txMessages": false, //optional - defaults to false,
-	
-		/* Magic value only required for setting up p2p block notifications. It is found in the daemon
-		   source code as the pchMessageStart variable.
-		   For example, litecoin mainnet magic: http://git.io/Bi8YFw
-		   And for litecoin testnet magic: http://git.io/NXBYJA */
-		 "peerMagic": "f9beb4d9", //optional
-		 "peerMagicTestnet": "0b110907" //optional
+
+		/**
+		 * Magic value only required for setting up p2p block notifications. It is found in the daemon
+		 * source code as the pchMessageStart variable.
+		 * For example, litecoin mainnet magic: http://git.io/Bi8YFw
+		 * And for litecoin testnet magic: http://git.io/NXBYJA
+		 */
+		"peerMagic": "f9beb4d9", //optional
+		"peerMagicTestnet": "0b110907" //optional
 	},
 
-    "address": process.env.REWARDS_ADDRESS, //Address to where block rewards are given
+	// address to where block rewards are given
+	"address": process.env.REWARDS_ADDRESS,
 
-    /* Block rewards go to the configured pool wallet address to later be paid out to miners,
-       except for a percentage that can go to, for examples, pool operator(s) as pool fees or
-       or to donations address. Addresses or hashed public keys can be used. Here is an example
-       of rewards going to the main pool op, a pool co-owner, and NOMP donation. */
-    "rewardRecipients": {
-        [process.env.REWARDS_ADDRESS]: 1.5, //1.5% goes to pool op
-        [process.env.REWARDS_ADDRESS]: 0.5, //0.5% goes to a pool co-owner
+	/**
+	 * Block rewards go to the configured pool wallet address to later be paid out to miners,
+	 * except for a percentage that can go to, for examples, pool operator(s) as pool fees or
+	 * or to donations address. Addresses or hashed public keys can be used. Here is an example
+	 * of rewards going to the main pool op, a pool co-owner, and NOMP donation.
+	 */
+	"rewardRecipients": {
+		[process.env.REWARDS_ADDRESS]: 1.5, // 1.5% goes to pool op
+	},
 
-        /* 0.1% donation to NOMP. This pubkey can accept any type of coin, please leave this in
-           your config to help support NOMP development. */
-		[process.env.REWARDS_ADDRESS]: 0.1
-    },
+	// how often to poll RPC daemons for new blocks, in milliseconds
+	"blockRefreshInterval": 1000,
 
-    "blockRefreshInterval": 1000, //How often to poll RPC daemons for new blocks, in milliseconds
+	/**
+	 * Some miner apps will consider the pool dead/offline if it doesn't receive anything new jobs
+	 * for around a minute, so every time we broadcast jobs, set a timeout to rebroadcast
+	 * in this many seconds unless we find a new job. Set to zero or remove to disable this.
+	 */
+	"jobRebroadcastTimeout": 55,
 
+	/**
+	 * Some attackers will create thousands of workers that use up all available socket connections,
+	 * usually the workers are zombies and don't submit shares after connecting. This features
+	 * detects those and disconnects them.
+	 * 
+	 * Remove workers that haven't been in contact for this many seconds
+	 */
+	"connectionTimeout": 600,
 
-    /* Some miner apps will consider the pool dead/offline if it doesn't receive anything new jobs
-       for around a minute, so every time we broadcast jobs, set a timeout to rebroadcast
-       in this many seconds unless we find a new job. Set to zero or remove to disable this. */
-    "jobRebroadcastTimeout": 55,
+	// Sometimes you want the block hashes even for shares that aren't block candidates.
+	"emitInvalidBlockHashes": false,
 
-    //instanceId: 37, //Recommend not using this because a crypto-random one will be generated
+	/**
+	 * Enable for client IP addresses to be detected when using a load balancer with TCP proxy
+	 * protocol enabled, such as HAProxy with 'send-proxy' param:
+	 * http://haproxy.1wt.eu/download/1.5/doc/configuration.txt
+	 */
+	"tcpProxyProtocol": false,
 
-    /* Some attackers will create thousands of workers that use up all available socket connections,
-       usually the workers are zombies and don't submit shares after connecting. This features
-       detects those and disconnects them. */
-    "connectionTimeout": 600, //Remove workers that haven't been in contact for this many seconds
-
-    /* Sometimes you want the block hashes even for shares that aren't block candidates. */
-    "emitInvalidBlockHashes": false,
-
-    /* Enable for client IP addresses to be detected when using a load balancer with TCP proxy
-       protocol enabled, such as HAProxy with 'send-proxy' param:
-       http://haproxy.1wt.eu/download/1.5/doc/configuration.txt */
-    "tcpProxyProtocol": false,
-
-    /* If a worker is submitting a high threshold of invalid shares we can temporarily ban their IP
-       to reduce system/network load. Also useful to fight against flooding attacks. If running
-       behind something like HAProxy be sure to enable 'tcpProxyProtocol', otherwise you'll end up
-       banning your own IP address (and therefore all workers). */
-    "banning": {
-        "enabled": true,
-        "time": 600, //How many seconds to ban worker for
-        "invalidPercent": 50, //What percent of invalid shares triggers ban
-        "checkThreshold": 500, //Check invalid percent when this many shares have been submitted
-        "purgeInterval": 300 //Every this many seconds clear out the list of old bans
-    },
-
-    /* Each pool can have as many ports for your miners to connect to as you wish. Each port can
-       be configured to use its own pool difficulty and variable difficulty settings. varDiff is
-       optional and will only be used for the ports you configure it for. */
-    "ports": {
-        [process.env.POOL_PORT]: { //A port for your miners to connect to
-            "diff": 32, //the pool difficulty for this port
-
-            /* Variable difficulty is a feature that will automatically adjust difficulty for
-               individual miners based on their hashrate in order to lower networking overhead */
-            "varDiff": {
-                "minDiff": 8, //Minimum difficulty
-                "maxDiff": 512, //Network difficulty will be used if it is lower than this
-                "targetTime": 15, //Try to get 1 share per this many seconds
-                "retargetTime": 90, //Check to see if we should retarget every this many seconds
-                "variancePercent": 30 //Allow time to very this % from target without retargeting
-            }
-        },
-        [process.env.POOL_PORT_NO_VARDIFF]: { //Another port for your miners to connect to, this port does not use varDiff
-            "diff": 256 //The pool difficulty
-        }
-    },
-
-    /* Recommended to have at least two daemon instances running in case one drops out-of-sync
-       or offline. For redundancy, all instances will be polled for block/transaction updates
-       and be used for submitting blocks. Creating a backup daemon involves spawning a daemon
-       using the "-datadir=/backup" argument which creates a new daemon instance with it's own
-       RPC config. For more info on this see:
-          - https://en.bitcoin.it/wiki/Data_directory
-          - https://en.bitcoin.it/wiki/Running_bitcoind */
-    "daemons": [
-        {   //Main daemon instance
-            "host": process.env.BLOQCOIN_HOST,
-            "port": process.env.BLOQCOIN_PORT,
-            "user": process.env.BLOQCOIN_USER,
-            "password": process.env.BLOQCOIN_PASS
-        },
-        /*
-        {   //Backup daemon instance
-            "host": "127.0.0.1",
-            "port": 19344,
-            "user": "bloqcoinrpc",
-            "password": "testnet"
-        }
-        */
-    ],
-
-
-    /* This allows the pool to connect to the daemon as a node peer to receive block updates.
-       It may be the most efficient way to get block updates (faster than polling, less
-       intensive than blocknotify script). It requires the additional field "peerMagic" in
-       the coin config. */
-    "p2p": {
+	/**
+	 * If a worker is submitting a high threshold of invalid shares we can temporarily ban their IP
+	 * to reduce system/network load. Also useful to fight against flooding attacks. If running
+	 * behind something like HAProxy be sure to enable 'tcpProxyProtocol', otherwise you'll end up
+	 * banning your own IP address (and therefore all workers).
+	 */
+	"banning": {
 		"enabled": true,
+		"time": 600, // how many seconds to ban worker for
+		"invalidPercent": 50, // what percent of invalid shares triggers ban
+		"checkThreshold": 500, // check invalid percent when this many shares have been submitted
+		"purgeInterval": 300 // every this many seconds clear out the list of old bans
+	},
 
-		/* Host for daemon */
+	/**
+	 * Each pool can have as many ports for your miners to connect to as you wish. Each port can
+	 * be configured to use its own pool difficulty and variable difficulty settings. varDiff is
+	 * optional and will only be used for the ports you configure it for.
+	 */
+	"ports": {
+		[process.env.POOL_PORT]: { // a port for your miners to connect to
+			"diff": 32, // the pool difficulty for this port
+
+			/**
+			 * Variable difficulty is a feature that will automatically adjust difficulty for
+			 * individual miners based on their hashrate in order to lower networking overhead
+			 */
+			"varDiff": {
+				"minDiff": 8, // minimum difficulty
+				"maxDiff": 512, // network difficulty will be used if it is lower than this
+				"targetTime": 15, // try to get 1 share per this many seconds
+				"retargetTime": 90, // check to see if we should retarget every this many seconds
+				"variancePercent": 30 // allow time to very this % from target without retargeting
+			}
+		},
+
+		[process.env.POOL_PORT_NO_VARDIFF]: { // another port for your miners to connect to, this port does not use varDiff
+			"diff": 256 // the pool difficulty
+		}
+	},
+
+	/**
+	 * Recommended to have at least two daemon instances running in case one drops out-of-sync
+	 * or offline. For redundancy, all instances will be polled for block/transaction updates
+	 * and be used for submitting blocks. Creating a backup daemon involves spawning a daemon
+	 * using the "-datadir=/backup" argument which creates a new daemon instance with it's own
+	 * RPC config. For more info on this see:
+	 * https://en.bitcoin.it/wiki/Data_directory
+	 * https://en.bitcoin.it/wiki/Running_bitcoind
+	 */
+	"daemons": [{
 		"host": process.env.BLOQCOIN_HOST,
+		"port": process.env.BLOQCOIN_PORT,
+		"user": process.env.BLOQCOIN_USER,
+		"password": process.env.BLOQCOIN_PASS
+	},
+	{
+		"host": process.env.BLOQCOIN_BACKUP_HOST,
+		"port": process.env.BLOQCOIN_BACKUP_PORT,
+		"user": process.env.BLOQCOIN_BACKUP_USER,
+		"password": process.env.BLOQCOIN_BACKUP_PASS
+	}],
 
-		/* Port configured for daemon (this is the actual peer port not RPC port) */
-		"port": process.env.BLOQCOIN_PORT_P2P,
+	/**
+	 * This allows the pool to connect to the daemon as a node peer to receive block updates.
+	 * It may be the most efficient way to get block updates (faster than polling, less
+	 * intensive than blocknotify script). It requires the additional field "peerMagic" in the coin config.
+	 */
+	"p2p": {
+		"enabled": true,
+		"host": process.env.BLOQCOIN_HOST, // host for daemon
+		"port": process.env.BLOQCOIN_PORT_P2P, // Port configured for daemon (this is the actual peer port not RPC port)
 
-		/* If your coin daemon is new enough (i.e. not a shitcoin) then it will support a p2p
-			feature that prevents the daemon from spamming our peer node with unnecessary
-			transaction data. Assume its supported but if you have problems try disabling it. */
+		/**
+		 * If your coin daemon is new enough (i.e. not a shitcoin) then it will support a p2p
+		 * feature that prevents the daemon from spamming our peer node with unnecessary
+		 * transaction data. Assume its supported but if you have problems try disabling it.
+		 */
 		"disableTransactions": true,
-
 		"magic": "f9beb4d9"
-    }
+	}
+}, function(ip, workerName, password, callback) { // stratum authorization function
 
-}, function(ip, workerName, password, callback){ //stratum authorization function
-    console.log("Authorize " + workerName + ":" + password + "@" + ip);
-    callback({
-        error: null,
-        authorized: true,
-        disconnect: false
-    });
+	console.log("Authorize " + workerName + ":" + password + "@" + ip);
+
+	// store this workerName for payout
+	
+	callback({
+		error: null,
+		authorized: true,
+		disconnect: false
+	});
 });
 
 /*
-
 'data' object contains:
     job: 4, //stratum work job ID
     ip: '71.33.19.37', //ip address of client
@@ -182,12 +191,12 @@ pool.on('share', function(isValidShare, isValidBlock, data) {
 	const obj = {};
 
 	obj.data = {
-		height: data.height, //block height
-		blockReward: data.blockReward, //the number of satoshis received as payment for solving this block
-		difficulty: data.difficulty, //stratum worker difficulty
-		shareDiff: data.shareDiff, //actual difficulty of the share
-		blockDiff: data.blockDiff, //block difficulty adjusted for share padding
-		blockDiffActual: data.blockDiffActual //actual difficulty for this block
+		height: data.height, // block height
+		blockReward: data.blockReward, // the number of satoshis received as payment for solving this block
+		difficulty: data.difficulty, // stratum worker difficulty
+		shareDiff: data.shareDiff, // actual difficulty of the share
+		blockDiff: data.blockDiff, // block difficulty adjusted for share padding
+		blockDiffActual: data.blockDiffActual // actual difficulty for this block
 	};
 
     if (isValidBlock) {
@@ -226,13 +235,13 @@ pool.on('share', function(isValidShare, isValidBlock, data) {
 	};
 });
 
-/*
-'severity': can be 'debug', 'warning', 'error'
-'logKey':   can be 'system' or 'client' indicating if the error
-            was caused by our system or a stratum client
-*/
+/**
+ * 'severity': can be 'debug', 'warning', 'error'
+ * 'logKey':   can be 'system' or 'client' indicating if the error was caused by our system or a stratum client
+ */
 pool.on('log', function(severity, logKey, logText){
-    console.log(severity + ': ' + '[' + logKey + '] ' + logText);
+	
+	console.log(severity + ': ' + '[' + logKey + '] ' + logText);
 });
 
 pool.start();
